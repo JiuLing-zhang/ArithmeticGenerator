58da442154a50dbf41a28173a91be00b7637074b
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-07-14 21:56:46 +0800

    等式选择器

---
+++ src/ArithmeticGenerator/Models/CustomNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace ArithmeticGenerator.Models;
+
+/// <summary>
+/// 数字
+/// </summary>
+public class CustomNumber
+{
+    /// <summary>
+    /// 整数位数
+    /// </summary>
+    public int Part1Length { get; set; }
+    /// <summary>
+    /// 小数位数
+    /// </summary>
+    public int Part2Length { get; set; }
+}
