5a62dae62a360d2aba593638f342b6de1de40c9b
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-07-14 23:06:38 +0800

    代码优化

--- src/ArithmeticGenerator/Models/CustomNumber.cs
+++ src/ArithmeticGenerator/Models/CustomNumber.cs
@@ -3,14 +3,25 @@
 /// <summary>
 /// 数字
 /// </summary>
-public class CustomNumber
+/// <param name="part1Length">整数位数</param>
+/// <param name="part2Length">小数位数</param>
+public class CustomNumber(int part1Length, int part2Length)
 {
     /// <summary>
     /// 整数位数
     /// </summary>
-    public int Part1Length { get; set; }
+    public int Part1Length { get; set; } = part1Length;
     /// <summary>
     /// 小数位数
     /// </summary>
-    public int Part2Length { get; set; }
+    public int Part2Length { get; set; } = part2Length;
+
+    public override string ToString()
+    {
+        if (Part2Length == 0)
+        {
+            return $"{Part1Length}位整数";
+        }
+        return $"{Part1Length}位整数{Part2Length}位小数";
+    }
 }
