f864ba0eca5c6255cdc94be2a97667123a8e4e30
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-08-05 11:27:29 +0800

    优化不能整除条件的生成逻辑

--- src/ArithmeticGenerator/QuestionBuilder/QuestionDivide.cs
+++ src/ArithmeticGenerator/QuestionBuilder/QuestionDivide.cs
@@ -13,7 +13,7 @@ internal class QuestionDivide(CustomNumber number1, CustomNumber number2) : Math
     {
         decimal value1;
         decimal value2;
-
+        Random rand = new Random();
         do
         {
             value1 = CreateNumberValue(Number1);
@@ -34,14 +34,40 @@ internal class QuestionDivide(CustomNumber number1, CustomNumber number2) : Math
                     return "不存在这样的等式！";
                 }
 
-                if (value1 % value2 != 0)
+                if (number1.Part2Length != Number2.Part2Length)
                 {
-                    continue;
+                    return "不存在这样的等式！";
                 }
+
+                int integerPartDigits;
+                int fractionalPartDigits;
+                do
+                {
+                    var value = rand.Next(1, 10);
+                    value1 = value2 * value;
+                    (integerPartDigits, fractionalPartDigits) = GetDecimalDigits(value1);
+                } while (integerPartDigits != Number1.Part1Length || fractionalPartDigits != Number1.Part2Length);
             }
             break;
         }
         while (true);
         return $"{value1} ÷ {value2} = ___";
+    }
+
+    private (int integerPartDigits, int fractionalPartDigits) GetDecimalDigits(decimal number)
+    {
+        string numberString = number.ToString();
+
+        int decimalPointIndex = numberString.IndexOf('.');
+
+        if (decimalPointIndex == -1)
+        {
+            return (numberString.Length, 0);
+        }
+
+        int integerPartDigits = decimalPointIndex;
+        int fractionalPartDigits = numberString.Length - decimalPointIndex - 1;
+
+        return (integerPartDigits, fractionalPartDigits);
     }
 }
