03616d70eb347f9167432df2c749ba7d36d7cf18
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-08-06 20:48:11 +0800

    优化符号

--- src/ArithmeticGenerator/QuestionBuilder/QuestionSubtract.cs
+++ src/ArithmeticGenerator/QuestionBuilder/QuestionSubtract.cs
@@ -33,6 +33,6 @@ internal class QuestionSubtract(CustomNumber number1, CustomNumber number2) : Ma
             value1 = CreateNumberValue(Number1);
             value2 = CreateNumberValue(Number2);
         }
-        return $"{value1} - {value2} = ___";
+        return $"{value1} － {value2} = ___";
     }
 }
